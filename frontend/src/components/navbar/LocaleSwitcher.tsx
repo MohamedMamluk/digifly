a72c60cb62717f52eeae070f3eeefdf4de57d1d3
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-08-15 23:24:32 +0300

    feat: optimize images

--- frontend/src/components/navbar/LocaleSwitcher.tsx
+++ frontend/src/components/navbar/LocaleSwitcher.tsx
@@ -2,25 +2,34 @@
 import React from 'react';
 import { useTranslations, useLocale } from 'next-intl';
 import { useRouter, usePathname } from '@/navigation';
+import Image from 'next/image';
 const flagImages = {
   ar: '/ar_flag.png',
   en: '/en_flag.png',
 };
+
 const LocaleSwitcher = () => {
   const pathname = usePathname();
-  const locale = useLocale();
+  const locale = useLocale() as 'en' | 'ar';
   const router = useRouter();
 
   const t = useTranslations('navbar');
   return (
     <button
-      className='flex items-center'
+      className='flex items-center  gap-2'
       onClick={() =>
         router.replace(pathname, { locale: locale == 'en' ? 'ar' : 'en' })
       }
     >
       {t('language')}
-      <img src={flagImages[locale]} alt='' className='w-8' />
+      <div className='relative w-8 aspect-square'>
+        <Image
+          fill
+          src={flagImages[locale]}
+          alt=''
+          className='w-full h-full object-contain'
+        />
+      </div>
     </button>
   );
 };
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,5 +1,7 @@
 import { useTranslations } from 'next-intl';
 import LocaleSwitcher from './LocaleSwitcher';
+import Image from 'next/image';
+import { Link } from '@/navigation';
 
 const Navbar = () => {
   const t = useTranslations('navbar');
@@ -7,12 +9,14 @@ const Navbar = () => {
   return (
     <nav className='bg-white border-gray-200 dark:bg-gray-900'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <a
-          href='https://flowbite.com/'
-          className='flex items-center space-x-3 rtl:space-x-reverse'
-        >
-          <img src='logo.png' className='w-28' alt='Flowbite Logo' />
-        </a>
+        <Link href='/' className='block relative w-full max-w-28 aspect-square'>
+          <Image
+            src='/logo.png'
+            className='w-full h-full object-cover'
+            alt='Digifly Logo'
+            fill
+          />
+        </Link>
         <button
           data-collapse-toggle='navbar-default'
           type='button'
@@ -40,38 +44,38 @@ const Navbar = () => {
         <div className='hidden w-max md:flex  gap-4' id='navbar-default'>
           <ul className='font-medium flex flex-col p-4 gap-4 md:p-0 mt-4 border items-center border-gray-100 rounded-lg bg-gray-50 md:flex-row   md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
-              <a
+              <Link
                 href='#'
                 className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
                 aria-current='page'
               >
                 {t('home')}
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href='#'
                 className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
               >
                 {t('categories')}
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href='#'
                 className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
               >
                 {t('about')}
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href='#'
                 className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
               >
                 {t('contact_us')}
-              </a>
+              </Link>
             </li>
           </ul>
           <LocaleSwitcher />
