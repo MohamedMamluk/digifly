7bde76c41b94a03ab363f29498c06ef08c5a3242
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-08-16 11:59:29 +0300

    style: move max-width to layout

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <nav className='bg-white border-gray-200 dark:bg-gray-900'>
-      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+      <div className=' flex flex-wrap items-center justify-between mx-auto p-4'>
         <Link href='/' className='block relative w-full max-w-28 aspect-square'>
           <Image
             src='/logo.png'
